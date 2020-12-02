ea7616fb331237c820acc8fc7c8402256597f483
Shardul Vaidya <cam.v737@gmail.com>
2020-12-02 01:15:51 -0500

    Added in boiler plate code to transmit using board pin 12

--- transmission.py
+++ transmission.py
@@ -1,5 +1,8 @@
-from binascii import hexlify, unhexlify
+from binascii import hexlify
+import Jetson.GPIO as GPIO
+import time
 
+output_pin = 18  # BCM pin 18, Board Pin 12
 
 def text_to_bits(text, encoding='ascii', errors='surrogatepass'):
     bits = bin(int(hexlify(text.encode(encoding, errors)), 16))[2:]
@@ -44,3 +47,31 @@ def convert_ascii_to_transmission_bits(text):
     bit_array = transmission_mask(bit_array)
 
     return bit_array
+
+
+def main(to_transmit):
+    # Pin Setup:
+    GPIO.setmode(GPIO.BCM)  # BCM pin-numbering as in Raspberry Pi
+
+    # set pin as an output pin with initial state low
+    GPIO.setup(output_pin, GPIO.OUT, initial=GPIO.LOW)
+
+    print("Starting transmission now")
+    transmission_bits = convert_ascii_to_transmission_bits()
+    print_bits(transmission_bits)
+
+    current_pos = 0
+    try:
+        while True:
+            if current_pos == len(transmission_bits):
+                current_pos = 0
+
+            GPIO.output(output_pin, transmission_bits[current_pos])
+            current_pos += 1
+            time.sleep(0.033333333)  # effectively a 30Hz transmission rate
+    finally:
+        GPIO.cleanup()
+
+
+if __name__ == '__main__':
+    main("hello world")
