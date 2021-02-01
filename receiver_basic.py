0a916174a1b9881a14ec77befd23c02b85f57cf3
Shardul Vaidya <cam.v737@gmail.com>
2021-02-01 17:17:45 -0500

    Zed Recorder custom script complete, python scripts complete

--- receiver_basic.py
+++ receiver_basic.py
@@ -1,23 +1,18 @@
-from binascii import unhexlify
-from enum import Enum
-
 import Jetson.GPIO as GPIO
-import subprocess as sp
 import time
 import logging
+import subprocess
 import sys
 import signal
 
 input_pin = 18  # BCM pin 18, BOARD pin 12
-cam = sl.Camera()  # Initialise Camera
+zed_recorder = subprocess.Popen(['python3', 'zed_record.py'])  # runs the ZED record script
 
 
 def interrupt_handler(sig, frame):
     print("You've pressed Ctrl+C!")
-    # End camera recording
-    cam.disable_recording()
-    cam.close()
     logging.info("Program ending")
+    zed_recorder.send_signal(signal.SIGTERM)
     GPIO.cleanup(input_pin)
     sys.exit(0)
 
@@ -31,30 +26,13 @@ def main():
     # set pin as an input pin with initial state
     GPIO.setup(input_pin, GPIO.IN)
 
-    init = sl.InitParameters()
-    init.camera_resolution = sl.RESOLUTION.VGA
-    init.depth_mode = sl.DEPTH_MODE.NONE
-
-    status = cam.open(init)
-    if status != sl.ERROR_CODE.SUCCESS:
-        print(repr(status))
-        exit(1)
-
-    path_output = "./videos/{0}".format(time.localtime())
-    recording_param = sl.RecordingParameters(path_output, sl.SVO_COMPRESSION_MODE.H264)
-    err = cam.enable_recording(recording_param)
-    if err != sl.ERROR_CODE.SUCCESS:
-        print(repr(status))
-        exit(1)
-
-    runtime = sl.RuntimeParameters()
-
     print("Welcome to the receiver readout")
     print("Camera is recording")
     print("This will print out the receiver values and auto formats it into 2 bit chunks")
 
     value_array = []
     while True:
+        time.sleep(1/60)
 
 
 if __name__ == '__main__':
--- zed_record.py
+++ zed_record.py
@@ -1,1 +1,42 @@
 import pyzed.sl as sl
+import signal
+import sys
+import time
+
+cam = sl.Camera()  # Initialise Camera
+
+
+def interrupt_handler(sig, frame):
+    # End camera recording
+    cam.disable_recording()
+    cam.close()
+    sys.exit(0)
+
+
+def main():
+    signal.signal(signal.SIGTERM, interrupt_handler)
+    init = sl.InitParameters()
+    init.camera_resolution = sl.RESOLUTION.VGA
+    init.depth_mode = sl.DEPTH_MODE.NONE
+
+    status = cam.open(init)
+    if status != sl.ERROR_CODE.SUCCESS:
+        print(repr(status))
+        exit(1)
+
+    path_output = "./videos/{0}".format(time.localtime())
+    recording_param = sl.RecordingParameters(path_output, sl.SVO_COMPRESSION_MODE.H264)
+    err = cam.enable_recording(recording_param)
+    if err != sl.ERROR_CODE.SUCCESS:
+        print(repr(status))
+        exit(1)
+
+    runtime = sl.RuntimeParameters()
+
+    while True:
+        cam.grab(runtime)
+        time.sleep(1/100)
+
+
+if __name__ == '__main__':
+    main()
