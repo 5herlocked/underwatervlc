5beb07b0e299d2af03101193f20920712a7da6e3
Shardul Vaidya <cam.v737@gmail.com>
2021-02-24 15:23:15 -0500

    Fixed zed_record.py script to ensure it's functioning with the current setups

--- zed_record.py
+++ zed_record.py
@@ -1,7 +1,7 @@
 import pyzed.sl as sl
 import signal
 import sys
-import time
+from datetime import date
 
 cam = sl.Camera()  # Initialise Camera
 
@@ -17,25 +17,26 @@ def main():
     signal.signal(signal.SIGTERM, interrupt_handler)
     init = sl.InitParameters()
     init.camera_resolution = sl.RESOLUTION.VGA
+    init.camera_fps = 100
     init.depth_mode = sl.DEPTH_MODE.NONE
 
     status = cam.open(init)
     if status != sl.ERROR_CODE.SUCCESS:
-        print(repr(status))
+        print('Camera.open failed')
         exit(1)
 
-    path_output = "./videos/{0}".format(time.localtime())
+    path_output = "./{0}.svo".format(date.today())
     recording_param = sl.RecordingParameters(path_output, sl.SVO_COMPRESSION_MODE.H264)
     err = cam.enable_recording(recording_param)
     if err != sl.ERROR_CODE.SUCCESS:
-        print(repr(status))
+        print('Enable Recording failed')
         exit(1)
 
     runtime = sl.RuntimeParameters()
 
     while True:
-        cam.grab(runtime)
-        time.sleep(1/100)
+        if cam.grab(runtime) == sl.ERROR_CODE.SUCCESS:
+            print("Grabed")
 
 
 if __name__ == '__main__':
