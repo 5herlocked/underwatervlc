07aa549200118491e5315fd0119c8a9439ef54eb
Shardul Vaidya <cam.v737@gmail.com>
2021-05-07 23:34:58 -0400

    Fixed random changes made during testing

--- photodiode.py
+++ photodiode.py
@@ -23,25 +23,26 @@ chan = AnalogIn(ads, ADS.P3)
 # chan = AnalogIn(ads, ADS.P0, ADS.P1)
 
 frequency = 1
-datapoints = list()
+data_points = list()
 output_name = 'photodiode_{}Hz'.format(frequency)
 
 
-# def log_data():
-#     for deltaTime, value, voltage in datapoints:
-#         logging.info("{}: Value {}, Voltage {}".format(deltaTime, value, voltage))
-#
-#     with open(output_name + "_data.csv", mode='w') as csv_file:
-#         csv_file = csv.writer(csv_file)
-#         csv_file.writerow(['time', 'value', 'voltage'])
-#         for deltaTime, value, voltage in datapoints:
-#             csv_file.writerow([deltaTime, value, voltage])
+def log_data():
+    global data_points
+    for deltaTime, value, voltage in data_points:
+        logging.info("{}: Value {}, Voltage {}".format(deltaTime, value, voltage))
+
+    with open(output_name + "_data.csv", mode='w') as csv_file:
+        csv_file = csv.writer(csv_file)
+        csv_file.writerow(['time', 'value', 'voltage'])
+        for deltaTime, value, voltage in data_points:
+            csv_file.writerow([deltaTime, value, voltage])
 
 
 def interrupt_handler(sig, frame):
     print("You've pressed Ctrl+C!")
-    # print("Datapoints collected: {}".format()
-    # log_data()
+    print("Data points collected: {}".format())
+    log_data()
     logging.info("Program ending")
     sys.stdout.flush()
     sys.exit(0)
@@ -49,7 +50,7 @@ def interrupt_handler(sig, frame):
 
 def main(argv):
     signal.signal(signal.SIGINT, interrupt_handler)
-    global frequency, datapoints, output_name
+    global frequency, data_points, output_name
     try:
         opts, args = getopt.getopt(argv, "hf:o:", ["frequency=", "output="])
     except getopt.GetoptError:
@@ -66,7 +67,7 @@ def main(argv):
     logging.basicConfig(filename=(output_name + ".log"))
     print("Started Polling")
     while True:
-        datapoints.append((time.asctime(), chan.value, chan.voltage))
+        data_points.append((time.asctime(), chan.value, chan.voltage))
         time.sleep(1 / frequency)
 
 
